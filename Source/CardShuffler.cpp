ffec5a45aeb9d25cfe194731faefb343371d7fb6
capsocrates <josephholyheart@gmail.com>
2015-09-22 01:09:29 -0500

    Basic On/Off Filtering Added
    Next up is refactoring and a basic GUI for it.

--- Source/CardShuffler.cpp
+++ Source/CardShuffler.cpp
@@ -10,13 +10,116 @@
 
 #include "CardShuffler.hpp"
 
+#include "Cardsets.hpp"
+#include "Cardtypes.hpp"
+#include "RandomizerCard.hpp"
+
+#include "boost/range/algorithm/find.hpp"
+#include "boost/range/algorithm/find_if.hpp"
+
 namespace SM
 {
 namespace Dominion
 {
+
+class CardsetFilter : public CardFilter
+{
+    Cardsets set;
+    bool cardsetIsAllowed;
+    virtual auto checkFilter(const RandomizerCard& in) const -> bool
+    {
+        //should return true if the cardset is allowed and the incoming card has that cardset, OR true if the sets don't match
+        return (cardsetIsAllowed && in.set == set) || in.set != set;
+    }
+    virtual auto getUniqueID() const -> std::wstring
+    {
+        return std::wstring(cardsetIsAllowed ? L"enables " : L"disables ") + str_from_cardset(set);
+    }
+public:
+    explicit CardsetFilter(const Cardsets set, const bool cardsetIsAllowed)
+        : CardFilter()
+        , set(set)
+        , cardsetIsAllowed(cardsetIsAllowed)
+    {}
+};
+
+class CardtypeFilter : public CardFilter
+{
+    Cardtypes type;
+    bool cardtypeIsAllowed;
+    virtual auto checkFilter(const RandomizerCard& in) const -> bool
+    {
+        //should return true if the cardset is allowed and the incoming card has that cardset, OR true if the sets don't match
+        const bool types_found(boost::find(in.applicable_types, type) != boost::end(in.applicable_types));
+        return (cardtypeIsAllowed && types_found) || !types_found;
+    }
+    auto getUniqueID() const -> std::wstring
+    {
+        return std::wstring(cardtypeIsAllowed ? L"enables " : L"disables ") + str_from_cardtype(type);
+    }
+public:
+    explicit CardtypeFilter(const Cardtypes type, const bool cardtypeIsAllowed)
+        : CardFilter()
+        , type(type)
+        , cardtypeIsAllowed(cardtypeIsAllowed)
+    {}
+};
+
 CardShuffler::CardShuffler()
     : generator(engine())
 {
+    disableCardset(Cardsets::alchemy);
+    disableCardset(Cardsets::base);
+    disableCardset(Cardsets::cornucopia);
+    disableCardset(Cardsets::dark_ages);
+    disableCardset(Cardsets::guilds);
+    disableCardset(Cardsets::hinterlands);
+    disableCardset(Cardsets::intrigue);
+    disableCardset(Cardsets::promo);
+    disableCardset(Cardsets::prosperity);
+    disableCardset(Cardsets::seaside);
+}
+void CardShuffler::enableCardset(Cardsets in)
+{
+    //enabling a Cardset will actually be removing the filter that is keeping it from sticking around
+    auto filter_to_remove = std::unique_ptr<CardFilter>(new CardsetFilter{in, true});
+    if (filterExistsAlready(*filter_to_remove))
+    {
+        filters.erase(findFilter(*filter_to_remove));
+    }
+}
+void CardShuffler::disableCardset(Cardsets in)
+{
+    //disabling a Cardset means adding a filter that will exclude it
+    auto new_filter = std::unique_ptr<CardFilter>(new CardsetFilter{in, false});
+    if (!filterExistsAlready(*new_filter))
+        filters.emplace_back(std::move(new_filter));
+}
+void CardShuffler::enableCardtype(Cardtypes in)
+{
+    //enabling a Cardtype will actually be removing the filter that is keeping it from sticking around
+    auto filter_to_remove = std::unique_ptr<CardFilter>(new CardtypeFilter{in, true});
+    if (filterExistsAlready(*filter_to_remove))
+        filters.erase(findFilter(*filter_to_remove));
+}
+void CardShuffler::disableCardtype(Cardtypes in)
+{
+    //disabling a Cardtype means adding a filter that will exclude it
+    auto new_filter = std::unique_ptr<CardFilter>(new CardtypeFilter{in, false});
+    if (!filterExistsAlready(*new_filter))
+        filters.emplace_back(std::move(new_filter));
+}
+auto CardShuffler::findFilter(const CardFilter& filter) const -> filter_citr
+{
+    return boost::find_if(filters,
+                          [&filter](const auto& lhs)
+    {
+        return lhs->uniqueID() == filter.uniqueID();
+    });
+}
+auto CardShuffler::filterExistsAlready(const CardFilter& filter) const -> bool
+{
+    return findFilter(filter) != boost::end(filters);
 }
 
 }   //end namespace Dominion
--- Source/CardShuffler.hpp
+++ Source/CardShuffler.hpp
@@ -11,17 +11,43 @@
   ==============================================================================
 */
 
-#include <boost/range/adaptor/sliced.hpp>
+#include <boost/range/adaptor/filtered.hpp>
+#include <boost/range/adaptor/indirected.hpp>
+#include <boost/range/adaptor/copied.hpp>
+#include <boost/range/algorithm/copy.hpp>
+#include <boost/range/algorithm/find_if.hpp>
 #include <boost/range/algorithm/random_shuffle.hpp>
 #include <boost/range/sub_range.hpp>
 
 #include <functional>   //for std::bind
+#include <memory>
 #include <random>
 
 namespace SM
 {
 namespace Dominion
 {
+
+enum class Cardsets;
+enum class Cardtypes;
+class RandomizerCard;
+
+class CardFilter    //an interface
+{
+public:
+    auto operator()(const RandomizerCard& in) const -> bool
+    {
+        return checkFilter(in);
+    };
+
+    auto uniqueID() const -> std::wstring
+    {
+        return getUniqueID();
+    };
+private:
+    virtual auto checkFilter(const RandomizerCard& in) const -> bool = 0;
+    virtual auto getUniqueID() const->std::wstring = 0;
+};
 
 class CardShuffler
 {
@@ -30,7 +56,7 @@ public:
 
     template<typename RangeT>
     auto shuffle(RangeT& in,
-                 typename RangeT::size_type count) -> boost::sliced_range<RangeT>
+                 typename RangeT::size_type count) -> RangeT
     {
         typedef typename std::iterator_traits<typename RangeT::iterator>::difference_type diff_t;
         typedef typename std::make_unsigned<diff_t>::type udiff_t;
@@ -49,11 +75,43 @@ public:
             std::ref(distr),
             std::ref(generator));
 
-        return boost::random_shuffle(in, gen) | boost::adaptors::sliced(0, count);
+        auto one_filter = [](const CardFilter& filter, const RandomizerCard& card) -> bool
+        {
+            //will return true if the card should be filtered out
+            return !filter(card);
+        };
+
+        using namespace boost::adaptors;
+        auto all_filters = [&one_filter](const RandomizerCard& card, const auto& filters) -> bool
+        {
+            auto bind_one_filter = std::bind(one_filter, _1, std::ref(card));
+            return boost::find_if(filters | indirected, bind_one_filter) == boost::end(filters | indirected);
+        };
+
+        auto bind_all_filters = std::bind(all_filters, _1, std::ref(filters));
+
+        RangeT return_val = boost::copy_range<RangeT>(in | filtered(bind_all_filters));
+        boost::copy(boost::random_shuffle(return_val, gen) | sliced(0, count),
+                    begin(return_val));
+        return boost::copy_range<RangeT>(return_val | copied(0, count));
     }
+
+    void enableCardset(Cardsets in);
+    void disableCardset(Cardsets in);
+
+    void enableCardtype(Cardtypes in);
+    void disableCardtype(Cardtypes in);
 private:
     std::random_device engine;
     std::mt19937 generator;
+    typedef std::unique_ptr<CardFilter> filterT;
+    typedef std::vector<filterT> vec_filterT;
+    vec_filterT filters;
+    typedef vec_filterT::iterator filter_itr;
+    typedef vec_filterT::const_iterator filter_citr;
+
+    auto findFilter(const CardFilter& filter) const -> filter_citr;
+    auto filterExistsAlready(const CardFilter& filter) const -> bool;
 };  //end class CardShuffler
 
 }   //end namespace Dominion
