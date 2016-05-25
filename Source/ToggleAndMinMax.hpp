21f1c52efb3c6688e03121bd9af582a91e25a211
capsocrates <josephholyheart@gmail.com>
2016-05-24 21:44:36 -0600

    Beginning work on Toggle Component

--- Source/ToggleAndMinMax.hpp
+++ Source/ToggleAndMinMax.hpp
@@ -20,12 +20,22 @@ template<typename EnumT>
 class ToggleAndMinMax : public juce::Component
 {
 public:
-    ToggleAndMinMax(EnumT) {};
+    ToggleAndMinMax(EnumT)
+        : toggle(),
+          text("0") {};
     ~ToggleAndMinMax() {};
 
     auto paint (juce::Graphics&) -> void {};
     auto resized() -> void {};
 private:
+    juce::ToggleButton toggle;
+    juce::TextButton min_text;
+    juce::ArrowButton min_less_arrow;
+    juce::ArrowButton min_more_arrow;
+    juce::TextButton max_text;
+    juce::ArrowButton max_less_arrow;
+    juce::ArrowButton max_more_arrow;
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ToggleAndMinMax)
 };
 
